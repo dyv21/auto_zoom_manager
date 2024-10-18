4e6dab2d1f0b03de9a2e877e59040a839b623a02
dyv21 <denis@artkerr.com>
2024-10-18 12:12:58 +0300

    Start settings

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Zoom Manager</h1>
+      <p>Current Zoom <b>{100}%</b></p>
+      <div> Zoom Controls</div>
+      <div>QuickZoom buttons</div>
+      <div> ScreenSizeZoomSettings</div>
+    </div>
+  )
+}
+
+export default App
